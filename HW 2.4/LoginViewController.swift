02683e3c149d43e34d27aa83bb69033f9c9e49b8
Zhenya <Henry_blackburn@icloud.com>
2024-04-13 14:47:59 +0400

    Initial Commit

---
+++ HW 2.4/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.4
+//
+//  Created by Евгений on 13.04.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

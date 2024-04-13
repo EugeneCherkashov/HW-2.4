f4c8c7c3acd4e281ad3f51bfc210f40f0581d51d
Zhenya <Henry_blackburn@icloud.com>
2024-04-13 20:12:49 +0400

    The end

--- HW 2.4/LoginViewController.swift
+++ HW 2.4/LoginViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var forgotUsername: UIButton!
+    @IBOutlet var forgotPassword: UIButton!
+    
+    private let user = "111"
+    private let password = "222"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.user = user
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true) //  Не смог сделать, списал, даже в интернете не нашёл инфу
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard usernameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "wrong login or password",
+                message: "enter correct information",
+                textField: passwordTextField // подсмотрел, как указать то, что очищаем
+            )
+            return false
+            }
+        return true
+    }
+    
+    @IBAction func forgotPasswordOrLogin(_ sender: UIButton) {
+        
+        switch sender {
+        case forgotUsername:
+            showAlert(title: "Bruh", message: "Your login is \(user)")
+        default:
+            showAlert(title: "Bruh", message: "Your password is \(password)")
+        }
+    }
+    
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
 
-
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) { // тоже подсмотрел, как указать то, что очищаем
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
 }
 
---
+++ HW 2.4/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  HW 2.4
+//
+//  Created by Евгений on 13.04.2024.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(user)!"
+    }
+    
+
+   
+
+}
